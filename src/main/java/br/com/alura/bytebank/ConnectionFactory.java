4e2e81283c7e0bbb06e6a5c0d445e4c157e9712a
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-05-15 09:51:20 -0300

    Add connection pool feature

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,23 +1,36 @@
 package br.com.alura.bytebank;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConnectionFactory {
 
+	Dotenv dotenv = Dotenv.configure().load();
+	String user = dotenv.get("SQL_USER");
+	String password = dotenv.get("SQL_PASSWORD");
+
 	public Connection conecta() {
 		try {
-			Dotenv dotenv = Dotenv.configure().load();
-			String user = dotenv.get("SQL_USER");
-			String password = dotenv.get("SQL_PASSWORD");
-			return DriverManager
-					.getConnection("jdbc:mysql://localhost:3306/byte_bank?user=" + user + "&password=" + password);
+
+			return createDataSource().getConnection();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private HikariDataSource createDataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+		config.setUsername(user);
+		config.setPassword(password);
+		config.setMaximumPoolSize(10);
+
+		return new HikariDataSource(config);
 	}
 
 }
